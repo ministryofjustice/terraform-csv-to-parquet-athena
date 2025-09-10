cd5bee9ad4b469d0674bdf7cae154205421f1411
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-09-10 18:16:07 +0100

    added aws provider parameter

--- s3.tf
+++ s3.tf
@@ -3,11 +3,24 @@ module "s3_concept_data_uploads_bucket" {
   bucket_prefix      = "${var.name}-data-uploads-${var.environment}-"
   custom_kms_key     = var.kms_key_arn
   versioning_enabled = true
+  providers = {
+    aws.bucket-replication = aws
+  }
 
   # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
   ownership_controls = "BucketOwnerEnforced"
+
   # Refer to the below section "Replication" before enabling replication
   replication_enabled = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
+  }
 
   lifecycle_rule = [
     {
@@ -56,11 +69,23 @@ module "s3_concept_data_output_bucket" {
   bucket_prefix      = "${var.name}-data-output-${var.environment}-"
   custom_kms_key     = var.kms_key_arn
   versioning_enabled = true
-
+  providers = {
+    aws.bucket-replication = aws
+  }
   # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
   ownership_controls = "BucketOwnerEnforced"
+
   # Refer to the below section "Replication" before enabling replication
   replication_enabled = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
+  }
 
   lifecycle_rule = [
     {
